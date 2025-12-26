391a372389937b20638d266d05b86c8dcf1a8e78
koracan <koracan@163.com>
2025-12-26 13:00:54 +0800

    remove setFullScreen

--- harmony/immersive/src/main/ets/RNImmersiveModule.ts
+++ harmony/immersive/src/main/ets/RNImmersiveModule.ts
@@ -51,14 +51,14 @@ export class RNImmersiveModule extends AnyThreadTurboModule implements TM.RNImme
     // 2. 设置沉浸式状态
     try {
       if (on) {
-        // 开启沉浸式：隐藏系统栏 + 全屏
+        // 开启沉浸式：隐藏系统，不处理全屏
         await win.setWindowSystemBarEnable([]);
-        await win.setFullScreen(true);
+        // await win.setFullScreen(true);
         console.log('[RNImmersive] Immersive mode enabled');
       } else {
-        // 关闭沉浸式：显示系统栏 + 退出全屏
+        // 关闭沉浸式：显示系统栏，不处理全屏
         await win.setWindowSystemBarEnable(['status', 'navigation']);
-        await win.setFullScreen(false);
+        // await win.setFullScreen(false);
         console.log('[RNImmersive] Immersive mode disabled');
       }
     } catch (error: any) {
